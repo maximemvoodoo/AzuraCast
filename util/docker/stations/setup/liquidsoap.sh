98cd159c0524af31fe0a16fd4a1c428be02eff26
maximem <maximem@voodoo.io>
2026-02-26 18:05:48 +0100

    one more

--- util/docker/stations/setup/liquidsoap.sh
+++ util/docker/stations/setup/liquidsoap.sh
@@ -4,4 +4,4 @@ set -x
 
 sudo apt-get install -y opam build-essential pkg-config libpcre2-dev libgmp-dev libcurl4-gnutls-dev
 opam init -y
-opam exec -- opam install -y liquidsoap.2.3.3
+opam exec -- bash /bd_build/stations/setup/liquidsoap.sh -H
