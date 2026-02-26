d8ab3d889af8f917517a8100db999346f44674ae
maximem <maximem@voodoo.io>
2026-02-26 17:47:18 +0100

    more install fixes

--- util/docker/stations/setup/liquidsoap.sh
+++ util/docker/stations/setup/liquidsoap.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-sudo apt-get install -y opam build-essential pkg-config libpcre3-dev libgmp-dev libcurl4-gnutls-dev
+sudo apt-get install -y opam build-essential pkg-config libpcre2-dev libgmp-dev libcurl4-gnutls-dev
 opam init -y
 opam install -y liquidsoap.2.3.3
